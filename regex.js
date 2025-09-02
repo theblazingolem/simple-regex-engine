fda916aec7d12deeb9e89eb7df8e2bd9c1daf401
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-09-02 11:56:15 +0530

    added search functionality etc

--- regex.js
+++ regex.js
@@ -1,21 +1,47 @@
 function matchOne(pattern, text) {
     if (!pattern) return true; //'' matches any pattern
     if (!text) return false; // cant match if patten is ''
-    if (pattern == ".") return true; // . matches any text
+    if (pattern === ".") return true; // . matches any text
     return pattern === text;
 }
-
-function search(pattern, text) {}
-
-function match(pattern, text) {
-    if (pattern == "") return true; //'' pattern matches any text
-    else
-        return (
-            matchOne(pattern[0], text[0]) &&
-            match(pattern.slice(1), text.slice(1))
-        );
+function search(pattern, text) {
+    if (pattern[0] === "^") {
+        return match(pattern.slice(1), text);
+    } else {
+        return match(".*" + pattern, text);
+    }
 }
 
+function match(pattern, text) {
+    // console.log(pattern, text);
+    if (pattern === "") {
+        return true; //'' pattern matches any text
+    } else if (pattern === "$" && text === "") {
+        return true; // $ matches the end of string
+    } else if (pattern[1] === "?") {
+        return matchQuestion(pattern, text);
+    } else if (pattern[1] === "*") {
+        return matchStar(pattern, text);
+    } else {
+        return (
+            matchOne(pattern[0], text[0]) && // checks if first chars are equal
+            match(pattern.slice(1), text.slice(1)) // recursively passes the pattern and text without the first char
+        );
+    }
+}
+function matchStar(pattern, text) {
+    return (
+        (matchOne(pattern[0], text[0]) && match(pattern, text.slice(1))) ||
+        match(pattern.slice(2), text)
+    );
+}
+function matchQuestion(pattern, text) {
+    return (
+        (matchOne(pattern[0], text[0]) &&
+            match(pattern.slice(2), text.slice(1))) ||
+        match(pattern.slice(2), text)
+    );
+}
 module.exports = {
     matchOne,
     match,
--- test.js
+++ test.js
@@ -1,12 +1,20 @@
-import { matchOne, match } from "./regex.js";
+import { matchOne, match, search } from "./regex.js";
 
-// console.log(matchOne("a", "a"));
-// console.log(matchOne(".", "z"));
-// console.log(matchOne("", "h"));
-// console.log(matchOne("a", "b"));
-// console.log(matchOne("p", ""));
+console.log(matchOne("a", "a"));
+console.log(matchOne(".", "z"));
+console.log(matchOne("", "h"));
+console.log(matchOne("a", "b"));
+console.log(matchOne("p", ""));
 
 console.log(match("a.c", "abc"));
 console.log(match("", "abc"));
-console.log(match("a..", "abc"));
-console.log(match(".", ""));
+console.log(match("a", "abc"));
+
+console.log(search("^abc", "abc"));
+console.log(search("^abcd", "abcd"));
+console.log(search("bc", "abcd"));
+
+console.log(search("ab?c", "ac"));
+console.log(search("ab?c", "abc"));
+console.log(search("a?b?c?", "abc"));
+console.log(search("a?b?c?", ""));
