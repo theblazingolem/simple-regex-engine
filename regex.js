690776cc1a86976fb4532af08b9f0cf4ccb2da90
Blazing <160132339+theblazingolem@users.noreply.github.com>
2025-09-02 08:02:05 +0530

    same len char matching function & testing

--- regex.js
+++ regex.js
@@ -7,7 +7,14 @@ function matchOne(pattern, text) {
 
 function search(pattern, text) {}
 
-function match(pattern, text) {}
+function match(pattern, text) {
+    if (pattern == "") return true; //'' pattern matches any text
+    else
+        return (
+            matchOne(pattern[0], text[0]) &&
+            match(pattern.slice(1), text.slice(1))
+        );
+}
 
 module.exports = {
     matchOne,
--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-import { matchOne } from "./regex.js";
+import { matchOne, match } from "./regex.js";
 
-console.log(matchOne("a", "a"));
-console.log(matchOne(".", "z"));
-console.log(matchOne("", "h"));
-console.log(matchOne("a", "b"));
-console.log(matchOne("p", ""));
+// console.log(matchOne("a", "a"));
+// console.log(matchOne(".", "z"));
+// console.log(matchOne("", "h"));
+// console.log(matchOne("a", "b"));
+// console.log(matchOne("p", ""));
+
+console.log(match("a.c", "abc"));
+console.log(match("", "abc"));
+console.log(match("a..", "abc"));
+console.log(match(".", ""));
